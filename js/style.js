7f4fc19946c49cdd2914caa7a42b6a9a2c991889
YahiyaCodeTechSoft <yahiya@codetechsoft.com>
2022-07-05 10:40:18 -0700

    fourt-commit

--- js/style.js
+++ js/style.js
@@ -26,7 +26,8 @@ window.addEventListener("scroll", (event) => {
     }
 });
 
-var a = 0;
+// var a =  document.getElementById('num').innerHTML;
+var a =  0;
 let scroll = window.scrollY;
 if (scroll > 500) {
   $(window).scroll(function () {
@@ -54,3 +55,58 @@ if (scroll > 500) {
     a = 1;
   });
 }
+
+
+$(document).ready(function () {
+  $(".all1").owlCarousel({
+    loop: true,
+    navigation: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    items: 3,
+    mousewheel: false,
+    dots: true,
+    slideBy: 1,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      200: {
+        items: 1,
+        nav: false,
+      },
+      300: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 1,
+        nav: false,
+      },
+      700: {
+        items: 1,
+        nav: false,
+      },
+      900: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+        loop: true,
+      },
+    },
+  });
+});
+$(".slider__btnright").click(function () {
+  var owl = $(".all1");
+  owl.trigger("next.owl.carousel");
+});
+$(".slider__btnleft").click(function () {
+  var owl = $(".all1");
+  owl.trigger("prev.owl.carousel");
+});
